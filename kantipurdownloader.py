d89ad17da261562da2413c65ca891b213cf1c6de
Bikram <salik.adhikari@gmail.com>
2012-03-19 15:53:39 +0000

    all things are right inside the function now

--- kantipurdownloader.py
+++ kantipurdownloader.py
@@ -10,28 +10,32 @@ def date():
   now = datetime.datetime.now()
   return str(now.day) + str(now.month) + str(now.year)
 #kantipuractualurl = 'http://epaper.ekantipur.com/1732012/epaperpdf/1732012-md-hr-6.pdf'
+
 def totalpages():
   """Retruns the total pages of the paper by counting the pages from the xml file"""
   file = urlopen('http://epaper.ekantipur.com/ktpost/' + date() +'/pages.xml')
   data = file.read()
   return data.count('<page>')
+
 def pagelist():
   """Returns a list with all the url of teh individual pages"""
   pages = []
   for i in range(1,totalpages()+1):
     pages.append('http://epaper.ekantipur.com/' + date() + '/epaperpdf/' + date() +'-md-hr-'+str(i) +'.pdf')
   return pages
-writer = PdfFileWriter()
-pages = pagelist()
-for i in pages:
-	remoteFile = urlopen(Request(i)).read()
-	memoryFile = StringIO(remoteFile)
-	pdfFile = PdfFileReader(memoryFile)
-	for pageNum in xrange(pdfFile.getNumPages()):
-		currentPage = pdfFile.getPage(pageNum)
-		writer.addPage(currentPage)
 
+def pdfprinter():
+  """Prints the pdf output"""
+  writer = PdfFileWriter()
+  pages = pagelist()
+  for i in pages:
+    remoteFile = urlopen(Request(i)).read()
+    memoryFile = StringIO(remoteFile)
+    pdfFile = PdfFileReader(memoryFile)
+    currentPage = pdfFile.getPage(0) #just need the first page, we only have one page
+    writer.addPage(currentPage)
+  outputStream = open(date()+"(kantipur)"+".pdf","wb")
+  writer.write(outputStream)
+  outputStream.close()
 
-outputStream = open(date()+"(kantipur)"+".pdf","wb")
-writer.write(outputStream)
-outputStream.close()
+pdfprinter()
