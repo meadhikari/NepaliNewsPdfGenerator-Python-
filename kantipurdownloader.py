ea2168e73c65b3154f754edc33cb27c171831e15
Bikram <salik.adhikari@gmail.com>
2012-03-19 16:49:14 +0000

    commandline arguments support but for Kantipur only

--- kantipurdownloader.py
+++ kantipurdownloader.py
@@ -5,29 +5,32 @@ from pyPdf import PdfFileWriter, PdfFileReader
 import xml.etree
 from urllib2 import Request, urlopen
 from StringIO import StringIO 
-def date():
+import sys
+papername = sys.argv[1]
+def date():  
   """Returns the date in the format used by the url of the newspapers"""
   now = datetime.datetime.now()
   return str(now.day) + str(now.month) + str(now.year)
-#kantipuractualurl = 'http://epaper.ekantipur.com/1732012/epaperpdf/1732012-md-hr-6.pdf'
-#ktmpostacturlurl = 'http://epaper.ekantipur.com/ktpost/%1$s/epaperpdf/%1$s-md-hr-%2$d.pdf'
-def totalpages():
+def totalpages(papername):
   """Retruns the total pages of the paper by counting the pages from the xml file"""
-  file = urlopen('http://epaper.ekantipur.com/' + date() +'/pages.xml')
-  data = file.read()
-  return data.count('<page>')
+  if papername == "Kantipur":
+    file = urlopen('http://epaper.ekantipur.com/' + date() +'/pages.xml')
+    data = file.read()
+    print data.count('<page>')
 
-def pagelist():
+    return data.count('<page>')
+def pagelist(papername):
   """Returns a list with all the url of teh individual pages"""
   pages = []
-  for i in range(1,totalpages()+1):
-    pages.append('http://epaper.ekantipur.com/' + date() + '/epaperpdf/' + date() +'-md-hr-'+str(i) +'.pdf')
+  for i in range(1,totalpages(papername)+1):
+    if papername == "Kantipur":
+      pages.append('http://epaper.ekantipur.com/' + date() + '/epaperpdf/' + date() +'-md-hr-'+str(i) +'.pdf')
   return pages
 
-def pdfprinter():
+def pdfprinter(papername):
   """Prints the pdf output"""
   writer = PdfFileWriter()
-  pages = pagelist()
+  pages = pagelist(papername)
   for i in pages:
     remoteFile = urlopen(Request(i)).read()
     memoryFile = StringIO(remoteFile)
@@ -38,4 +41,4 @@ def pdfprinter():
   writer.write(outputStream)
   outputStream.close()
 
-pdfprinter()
+pdfprinter(papername)
