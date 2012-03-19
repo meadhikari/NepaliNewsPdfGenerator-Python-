62660c6a7ba36100a00870946205734a0e75c60f
Bikram <salik.adhikari@gmail.com>
2012-03-19 15:27:20 +0000

    initial working state

--- kantipurdownloader.py
+++ kantipurdownloader.py
@@ -1,17 +1,13 @@
 import datetime
-#import urllib2
 import re 
 import os
 from pyPdf import PdfFileWriter, PdfFileReader
 import xml.etree
 from urllib2 import Request, urlopen
-from StringIO import StringIO
-output = PdfFileWriter()
+from StringIO import StringIO 
 now = datetime.datetime.now()
 now = str(now.day) + str(now.month) + str(now.year)
-
-actualurl = 'http://epaper.ekantipur.com/1732012/epaperpdf/1732012-md-hr-6.pdf'
-
+kantipuractualurl = 'http://epaper.ekantipur.com/1732012/epaperpdf/1732012-md-hr-6.pdf'
 file = urlopen('http://epaper.ekantipur.com/ktpost/' + now +'/pages.xml')
 data = file.read()
 pages = []
@@ -19,13 +15,16 @@ count = data.count('<page>')
 for i in range(1,count+1):
   pages.append('http://epaper.ekantipur.com/' + now + '/epaperpdf/' +now +'-md-hr-'+str(i) +'.pdf')
 
-
-for url in pages:
-	remoteFile = urlopen(Request(url)).read()
+writer = PdfFileWriter()
+for i in pages:
+	remoteFile = urlopen(Request(i)).read()
 	memoryFile = StringIO(remoteFile)
 	pdfFile = PdfFileReader(memoryFile)
-        output.addPage(pdfFile.getPage(0))
-        outputStream = file("output.pdf","wb")
-        output.write(outputStream)
-        outputStream.close()
+	for pageNum in xrange(pdfFile.getNumPages()):
+		currentPage = pdfFile.getPage(pageNum)
+		writer.addPage(currentPage)
 
+
+outputStream = open(filename+"(kantipur)"+".pdf","wb")
+writer.write(outputStream)
+outputStream.close()
